67462fae86a9874e29b4ff3e455a0f8c8457e324
Carlos Gómez-Rodríguez <komoku@users.noreply.github.com>
2010-04-30 19:12:09 +0000

    - Fixed bug in client-server communication that would result in not receiving the visual configuration settings from the server. Fixes issue #59. - The remote connection window now shows the correct default port for an AGE connection. Fixes issue #60. - Fixed transparency issue with the remote connection window. - Changed AgeArena code file to UTF-8 so accents will display property in the current version of AGE.

---
+++ age/src/eu/irreality/age/util/xml/ArrayNodeList.java
@@ -0,0 +1,62 @@
+/*
+ * (c) 2005-2009 Carlos Gómez Rodríguez, todos los derechos reservados / all rights reserved.
+ * Licencia en license/bsd.txt / License in license/bsd.txt
+ */
+
+/*
+ * Created at regulus on 08-abr-2007 11:46:29
+ * as file ArrayNodeList.java on package org.f2o.absurdum.puck.util.xml
+ */
+package eu.irreality.age.util.xml;
+
+import java.util.ArrayList;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * A trivial implementation of a DOM XML node list.
+ * @author carlos
+ *
+ * Created at regulus, 08-abr-2007 11:46:29
+ */
+public class ArrayNodeList implements NodeList
+{
+
+	private ArrayList impl;
+	
+	public ArrayNodeList()
+	{
+		impl = new ArrayList();
+	}
+	
+	public void add ( Node n )
+	{
+		impl.add(n);
+	}
+	
+	public boolean remove ( Node n )
+	{
+		return impl.remove(n);
+	}
+	
+	public int getLength()
+	{
+		return impl.size();
+	}
+	
+	public Node item ( int index )
+	{
+		try
+		{
+			return (Node) impl.get(index);
+		}
+		catch ( IndexOutOfBoundsException ioobe )
+		{
+			//as strange as it may seem, the org.w3c.dom.NodeList interface's contract says
+			//this method should return null for invalid indexes
+			return null;
+		}
+	}
+	
+}
