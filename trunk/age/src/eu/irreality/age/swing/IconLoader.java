ad4728a3403d843807ae9f97186d8c800c610ce4
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2010-09-02 10:31:52 +0000

    Tried to fix error handling of SVG files. Failed. Basically no changes.

--- trunk/age/src/eu/irreality/age/swing/IconLoader.java
+++ trunk/age/src/eu/irreality/age/swing/IconLoader.java
@@ -1,5 +1,7 @@
 package eu.irreality.age.swing;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 
 import javax.swing.Icon;
@@ -13,17 +15,28 @@ public class IconLoader
 
     public static Icon loadIcon ( java.net.URL url )
     {
-	if ( url.toString().toLowerCase().endsWith("svg") )
-	{
-	    URI uri = SVGCache.getSVGUniverse().loadSVG(url);
-	    SVGIcon icon = new SVGIcon();
-	    icon.setSvgURI(uri);
-	    return icon;
-	}
-	else
-	{
-	    return new ImageIcon(url);
-	}
+		if ( url.toString().toLowerCase().endsWith("svg") )
+		{
+			//try
+			//{
+				URI uri = SVGCache.getSVGUniverse().loadSVG(url);
+				SVGIcon icon = new SVGIcon();
+				icon.setSvgURI(uri);
+				return icon;
+			//}
+			//catch ( Exception e ) //it doesn't throw exceptions
+			//{
+			//	System.err.println("Warning, SVG not found or could not be read: " + url);
+			//	return null;
+			//}
+		}
+		else
+		{
+			ImageIcon ii = new ImageIcon(url);
+			if ( ii == null )
+				System.err.println("Warning, image not found or could not be read: " + url);
+		    return ii;
+		}
     }
     
 }
