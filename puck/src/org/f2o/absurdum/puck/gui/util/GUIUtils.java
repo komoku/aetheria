f5762dd3e072e404f2d63af03b11f95d4586dd8a
Carlos Gómez-Rodríguez <komoku@users.noreply.github.com>
2011-02-05 10:35:58 +0000

    Nice layout changes to PUCK description panels.

---
+++ puck/src/org/f2o/absurdum/puck/gui/util/GUIUtils.java
@@ -0,0 +1,33 @@
+/**
+ * (c) 2000-2011 Carlos Gómez Rodríguez, todos los derechos reservados / all rights reserved.
+ * Licencia en license.txt / License in license.txt
+ * File created: 05/02/2011 11:25:28
+ */
+package org.f2o.absurdum.puck.gui.util;
+
+import java.awt.Dimension;
+
+import javax.swing.JComponent;
+
+/**
+ * @author carlos
+ *
+ */
+public class GUIUtils 
+{
+
+	public static void limitVertically ( JComponent comp )
+	{
+		int prefH = (int) comp.getPreferredSize().getHeight();
+		int maxW = (int) comp.getMaximumSize().getWidth();
+		comp.setMaximumSize(new Dimension(maxW,prefH));
+	}
+	
+	public static void limitHorizontally ( JComponent comp )
+	{
+		int prefW = (int) comp.getPreferredSize().getWidth();
+		int maxH = (int) comp.getMaximumSize().getHeight();
+		comp.setMaximumSize(new Dimension(prefW,maxH));
+	}
+	
+}
