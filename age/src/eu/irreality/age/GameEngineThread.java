b5630b1d15e5578b5d20afd4cf64412280a8f043
Carlos Gómez-Rodríguez <komoku@users.noreply.github.com>
2011-03-20 16:58:16 +0000

    Changed version numbers to 1.0.3.

--- age/src/eu/irreality/age/GameEngineThread.java
+++ age/src/eu/irreality/age/GameEngineThread.java
@@ -18,12 +18,12 @@ public class GameEngineThread extends Thread
 
 	public static String getVersion() 
 	{ 
-		return "Aetheria Game Engine v 1.0.2";
+		return "Aetheria Game Engine v 1.0.3";
 	}
 	
 	public static String getVersionNumber()
 	{
-		return "1.0.2";
+		return "1.0.3";
 	}
 
 	
