0f38a4e1df0f1d0942d33b8ebc470ee981cb4570
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2011-06-19 16:08:55 +0000

    Worlds in zip files (with extensions zip, jar or agz) can now be opened by all clients EXCEPT FOR the applet client (the latter caused security exceptions).
    Resolves issue #88.

--- trunk/age/src/eu/irreality/age/FiltroFicheroMundo.java
+++ trunk/age/src/eu/irreality/age/FiltroFicheroMundo.java
@@ -8,13 +8,14 @@ public class FiltroFicheroMundo extends javax.swing.filechooser.FileFilter
 	public boolean accept ( File f )
 	{
 		if ( f.getName().equalsIgnoreCase("world.dat") || f.getName().equalsIgnoreCase("world.xml") ) return true;
+		else if ( f.getName().endsWith(".agz") ) return true;
 		else if ( !f.isFile() ) return true;
 		else return false;
 	}
 	
 	public String getDescription()
 	{
-		return "Ficheros de mundo de AGE (world.dat, world.xml)";
+		return "Ficheros de mundo de AGE (world.xml,*.agz)";
 	}
 	
 }
