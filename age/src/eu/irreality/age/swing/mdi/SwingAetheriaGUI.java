95c2c932db3da99da58e76ae460aa80a43295d8c
Carlos Gómez-Rodríguez <komoku@users.noreply.github.com>
2011-07-10 15:10:25 +0000

    Changed version number to 1.1.5.

--- age/src/eu/irreality/age/swing/mdi/SwingAetheriaGUI.java
+++ age/src/eu/irreality/age/swing/mdi/SwingAetheriaGUI.java
@@ -142,7 +142,7 @@ public class SwingAetheriaGUI extends JFrame
 		}
 		 */
 
-		super("Aetheria Game Engine, v 1.1.4");
+		super("Aetheria Game Engine, v 1.1.5");
 		instance = this;
 		
 		//obsolete
