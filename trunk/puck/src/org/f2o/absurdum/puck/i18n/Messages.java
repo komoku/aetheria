e42b10aaabce2dd36a1a347d2c5fc4355d622e0c
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2011-11-12 12:44:19 +0000

    Puck Messages and AGE UIMessages classes are now locale-sensitive. Still missing the controls/menus to change language.

--- trunk/puck/src/org/f2o/absurdum/puck/i18n/Messages.java
+++ trunk/puck/src/org/f2o/absurdum/puck/i18n/Messages.java
@@ -9,7 +9,12 @@ package org.f2o.absurdum.puck.i18n;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 import java.util.Properties;
+
+import org.f2o.absurdum.puck.gui.config.PuckConfiguration;
+
+import eu.irreality.age.swing.config.AGEConfiguration;
 
 /**
  * @author carlos
@@ -23,13 +28,51 @@ public /*Singleton*/ class Messages
 	
 	private Properties properties;
 	
+	/**
+	 * Obtains the language code for the user's preferred language as configured in the AGE configuration file.
+	 * If no language is configured in the file, then if the JVM locale is Spanish or English, it returns that language.
+	 * In other case, it returns Spanish.
+	 * @return
+	 */
+	private String getPreferredLanguage()
+	{
+		String configuredLanguage = PuckConfiguration.getInstance().getProperty("language");
+		if ( configuredLanguage != null ) return configuredLanguage;
+		String sysLanguage = Locale.getDefault().getLanguage();
+		if ( sysLanguage.contains("es") )
+		{
+			PuckConfiguration.getInstance().setProperty("language","es");
+			return "es";
+		}
+		if ( sysLanguage.contains("en") )
+		{
+			PuckConfiguration.getInstance().setProperty("language","en");
+			return "en";
+		}
+		return "es";
+	}
+	
 	private Messages()
+	{
+		String lang = getPreferredLanguage();
+		try
+		{
+			init("org/f2o/absurdum/puck/i18n/Messages."+lang);
+		}
+		catch ( Exception exc )
+		{
+			System.err.println("Could not load PUCK UI message file for language " + lang + ", will try with the default message file.\n");
+			init("org/f2o/absurdum/puck/i18n/Messages.properties");
+		}
+	}
+	
+	public void init ( String pathToMessageFile )
 	{	
 		properties = new Properties();
 		try
 		{
-			InputStream is = this.getClass().getClassLoader().getResourceAsStream("org/f2o/absurdum/puck/i18n/Messages.properties");
-			if ( is == null ) throw new IOException("getResourceAsStream returned null stream for Messages.properties");
+			InputStream is = this.getClass().getClassLoader().getResourceAsStream(pathToMessageFile);
+			if ( is == null ) throw new IOException("getResourceAsStream returned null stream for " + pathToMessageFile);
 			properties.load( is );
 		}
 		catch ( IOException ioe )
