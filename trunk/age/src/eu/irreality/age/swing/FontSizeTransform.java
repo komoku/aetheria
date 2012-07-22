8ea21fe7a8a895000f4696402606f0e78d02d79c
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2012-07-22 19:34:35 +0000

    Changed the method created in the previous commit to scaleFonts(double), which applies a zoom factor to all the fonts, and made the internal code somewhat more generic.

---
+++ trunk/age/src/eu/irreality/age/swing/FontSizeTransform.java
@@ -0,0 +1,42 @@
+package eu.irreality.age.swing;
+
+/**
+ * Defines a transformation on font size. Can be adding an offset, or multiplying by a number (i.e. zooming)
+ * @author carlos
+ *
+ */
+public class FontSizeTransform 
+{
+
+	public static int ADD = 0;
+	public static int MULTIPLY = 1;
+	
+	private int type = ADD;
+	private double param = 4.0;
+	
+	/**
+	 * Creates a new FontSizeTransform. If type is FontSizeTransform.ADD, then the transform adds param as a
+	 * (positive or negative) offset to font size. If type is FontSizeTransform.MULTIPLY, then the transform
+	 * scales font size by the factor given in param.
+	 * @param type
+	 * @param param
+	 */
+	public FontSizeTransform ( int type , double param )
+	{
+		this.type = type;
+		this.param = param;
+	}
+	
+	public int apply ( int size )
+	{
+		if ( type == ADD )
+		{
+			return size + (int)param;
+		}
+		else //if ( type == MULTIPLY )
+		{
+			return (int) ( size * param );
+		}
+	}
+	
+}
