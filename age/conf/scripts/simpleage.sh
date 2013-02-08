45e200f7f381212a905384b6e651c935d1c0c83f
Carlos Gómez-Rodríguez <komoku@users.noreply.github.com>
2013-02-08 22:38:29 +0000

    Updated Linux scripts to accept command-line arguments.

--- age/conf/scripts/simpleage.sh
+++ age/conf/scripts/simpleage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Llamando a la Java (tm) Virtual Machine para ejecutar Aetheria..."
-java -Xmx512M -classpath AgeCore.jar:lib/bsh-2.0b4.jar:lib/micromod.jar:lib/commons-logging-api.jar:lib/jl1.0.jar:lib/jogg-0.0.7.jar:lib/jorbis-0.0.15.jar:lib/jspeex0.9.7.jar:lib/tritonus_share.jar:lib/vorbisspi1.0.2.jar:lib/mp3spi1.9.4.jar:lib/basicplayer3.0.jar:lib/svgSalamander.jar:lib/commons-cli-1.2.jar eu.irreality.age.swing.sdi.SwingSDIInterface > /dev/null
+java -Xmx512M -classpath AgeCore.jar:lib/bsh-2.0b4.jar:lib/micromod.jar:lib/commons-logging-api.jar:lib/jl1.0.jar:lib/jogg-0.0.7.jar:lib/jorbis-0.0.15.jar:lib/jspeex0.9.7.jar:lib/tritonus_share.jar:lib/vorbisspi1.0.2.jar:lib/mp3spi1.9.4.jar:lib/basicplayer3.0.jar:lib/svgSalamander.jar:lib/commons-cli-1.2.jar eu.irreality.age.SwingAetheriaGameLoaderInterface -sdi $* > /dev/null
