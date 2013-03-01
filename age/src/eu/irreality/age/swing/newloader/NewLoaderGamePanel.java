83a7309efbc13860d993b0a3302025666339bbdf
Carlos Gómez-Rodríguez <komoku@users.noreply.github.com>
2013-03-01 17:43:36 +0000

    Mode code for the experimental loader. It's now even possible to *attempt* to download a game.

--- age/src/eu/irreality/age/swing/newloader/NewLoaderGamePanel.java
+++ age/src/eu/irreality/age/swing/newloader/NewLoaderGamePanel.java
@@ -1,17 +1,26 @@
 package eu.irreality.age.swing.newloader;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.xml.transform.TransformerException;
+
+import eu.irreality.age.i18n.UIMessages;
 
 
 /**
@@ -23,10 +32,33 @@ public class NewLoaderGamePanel extends JPanel
 {
 	
 	private JTable gameTable;
-	private JTextPane infoPane;
+	private JTextPane infoPane = new JTextPane();
 	private GameTableModel gameTableModel;
 	private XTableColumnModel gameTableColumnModel;
 	private JScrollPane tableScrollPane;
+	private JPanel downloadOrPlayButtonPanel = new JPanel();
+	private JButton currentDownloadOrPlayButton; //this can be a play button or a download button
+	
+	private JButton playButton = new JButton("Play");
+	private JButton downloadButton = new JButton("Download");
+	
+	
+	private GameEntry getSelectedGameEntry()
+	{
+		int index = gameTable.getSelectedRow();
+		return (GameEntry) gameTableModel.getGameEntry(index);
+	}
+	
+	public void downloadSelected() throws IOException
+	{
+		GameEntry toDownload = getSelectedGameEntry();
+		toDownload.download();
+	}
+	
+	private void showError(String message, String title)
+	{
+		JOptionPane.showMessageDialog(this,"<html><p>"+message+"</p>",title,JOptionPane.ERROR_MESSAGE);
+	}
 	
 	public NewLoaderGamePanel()
 	{
@@ -40,7 +72,7 @@ public class NewLoaderGamePanel extends JPanel
 		} 
 		catch (Exception e)
 		{
-			JOptionPane.showMessageDialog(this,"<html><p>"+e.getLocalizedMessage()+"</p>","Whoops!",JOptionPane.ERROR_MESSAGE);
+			showError(e.getLocalizedMessage(),"Whoops!");
 			e.printStackTrace();
 		}
 		gameTableColumnModel = new XTableColumnModel();
@@ -48,16 +80,126 @@ public class NewLoaderGamePanel extends JPanel
 		gameTable = new JTable ( gameTableModel , gameTableColumnModel );
 		gameTable.createDefaultColumnsFromModel();
 		gameTableColumnModel.setAllColumnsVisible();
+		gameTableColumnModel.setColumnVisible(gameTableColumnModel.getColumnByModelIndex(2),false);
+		gameTableColumnModel.setColumnVisible(gameTableColumnModel.getColumnByModelIndex(3),false);
+		gameTableColumnModel.setColumnVisible(gameTableColumnModel.getColumnByModelIndex(4),false);
+		gameTableColumnModel.setColumnVisible(gameTableColumnModel.getColumnByModelIndex(5),false);
+		gameTableColumnModel.setColumnVisible(gameTableColumnModel.getColumnByModelIndex(7),false);
+		gameTableColumnModel.setColumnVisible(gameTableColumnModel.getColumnByModelIndex(8),false);
 		
 		tableScrollPane = new JScrollPane(gameTable);
 		gameTable.setFillsViewportHeight(true);
 		gameTable.setShowHorizontalLines(false);
 		
+		ListSelectionModel selModel = gameTable.getSelectionModel();
+		selModel.addListSelectionListener ( new ListSelectionListener()
+		{
+			public void valueChanged(ListSelectionEvent e) 
+			{
+				//Ignore extra messages.
+                if (e.getValueIsAdjusting()) return;
+
+                ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+                if (lsm.isSelectionEmpty()) 
+                {
+                    //no rows are selected - do nothing.
+                } 
+                else 
+                {
+                    int selectedRow = lsm.getMinSelectionIndex();
+                    showGameEntry ( (GameEntry) gameTableModel.getGameEntry(selectedRow) );
+                }	
+			}	
+		}
+				);
+		gameTable.setRowSelectionInterval(0,0);
+		
+		
 		add(tableScrollPane);
-		add(new JLabel("test"));
+		
+		JPanel rightPanel = new JPanel();
+		rightPanel.setLayout(new BoxLayout(rightPanel,BoxLayout.PAGE_AXIS));
+		
+		rightPanel.add(infoPane);
+		
+		JPanel rightDownPanel = new JPanel();
+		rightDownPanel.setLayout(new BoxLayout(rightDownPanel,BoxLayout.LINE_AXIS));
+		
+		rightPanel.add(rightDownPanel);
+		
+		rightDownPanel.add(Box.createHorizontalGlue());
+		rightDownPanel.add(downloadOrPlayButtonPanel);
+		
+		add(rightPanel);
+		
+		downloadButton.addActionListener( new ActionListener()
+		{
+			public void actionPerformed ( ActionEvent e )
+			{
+				try 
+				{
+					downloadSelected();
+				} catch (IOException e1) 
+				{
+					showError(e1.getLocalizedMessage(),"Whoops!");
+					e1.printStackTrace();
+				}
+			}
+		}
+				);
+		
 		
 		//System.err.println(gameTable.getRowCount());
 		//System.err.println(gameTable.getValueAt(0, 0));
+		
+	}
+	
+	private void activatePlayButton()
+	{
+		if ( currentDownloadOrPlayButton != null && currentDownloadOrPlayButton != playButton )
+		{
+			downloadOrPlayButtonPanel.remove(currentDownloadOrPlayButton);
+			currentDownloadOrPlayButton = null;
+		}
+		if ( currentDownloadOrPlayButton == null )
+		{
+			downloadOrPlayButtonPanel.add(playButton);
+			currentDownloadOrPlayButton = playButton;
+		}
+	}
+	
+	private void activateDownloadButton()
+	{
+		if ( currentDownloadOrPlayButton != null && currentDownloadOrPlayButton != downloadButton )
+		{
+			downloadOrPlayButtonPanel.remove(currentDownloadOrPlayButton);
+			currentDownloadOrPlayButton = null;
+		}
+		if ( currentDownloadOrPlayButton == null )
+		{
+			downloadOrPlayButtonPanel.add(downloadButton);
+			currentDownloadOrPlayButton = downloadButton;
+		}
+	}
+	
+	private void showGameEntry ( GameEntry ge )
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append( UIMessages.getInstance().getMessage("gameinfo.name") + ":" + ge.getTitle() + "\n" );
+		sb.append( UIMessages.getInstance().getMessage("gameinfo.author") + ":" + ge.getAuthor() + "\n" );
+		sb.append( UIMessages.getInstance().getMessage("gameinfo.date") + ":" + ge.getDate() + "\n" );
+		sb.append( UIMessages.getInstance().getMessage("gameinfo.version") + ":" + ge.getVersion() + "\n" );
+		sb.append( UIMessages.getInstance().getMessage("gameinfo.required") + ":" + ge.getAgeVersion() + "\n" );
+		sb.append( UIMessages.getInstance().getMessage("gameinfo.downloaded") + ":" + ge.isDownloaded() + "\n" );
+		
+		infoPane.setText(sb.toString());
+		
+		//and show the play button or the download button as needed.
+		if ( ge.isDownloaded() )
+			activatePlayButton();
+		else
+			activateDownloadButton();
 		
 	}
 	
