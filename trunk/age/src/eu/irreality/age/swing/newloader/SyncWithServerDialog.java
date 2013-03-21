490c24e2320f7732e5d1ecc3b12b09e6cd814297
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2013-03-21 16:34:19 +0000

    Continued programming sync dialog.

--- trunk/age/src/eu/irreality/age/swing/newloader/SyncWithServerDialog.java
+++ trunk/age/src/eu/irreality/age/swing/newloader/SyncWithServerDialog.java
@@ -2,18 +2,32 @@ package eu.irreality.age.swing.newloader;
 
 import java.awt.Frame;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import eu.irreality.age.i18n.UIMessages;
+import eu.irreality.age.swing.config.AGEConfiguration;
 
 public class SyncWithServerDialog extends JDialog 
 {
+	
+	private JButton buttonOk = new JButton("button.ok");
+	private JButton buttonCancel = new JButton("button.can");
+	private JButton buttonChangeUrl = new JButton("gameloader.change.url");
 
 	public SyncWithServerDialog ( Frame parent , boolean modal )
 	{
 		super(parent,modal);
 		setTitle(UIMessages.getInstance().getMessage("gameloader.sync"));
+		getContentPane().setLayout(new BoxLayout(getContentPane(),BoxLayout.PAGE_AXIS));
+		getContentPane().add(new JLabel(UIMessages.getInstance().getMessage("gameloader.sync.description")));
+		getContentPane().add(new JLabel(AGEConfiguration.getInstance().getProperty("catalogURL")));
+		//TODO add panel with buttons here
+		pack();
+		setVisible(true);
 	}
 	
 }
