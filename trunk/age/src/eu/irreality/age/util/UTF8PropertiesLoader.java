2e135d38bfd07a0596f860d5955715487ab41bb6
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2013-04-09 21:03:32 +0000

    Fixed bug by which the hack to read properties files encoded in UTF-8 didn't work correctly when the files contained non-ISO characters. The hack now works with those files and AGE is still compatible with java 1.5, although it's definitely worth thinking about dropping that compatibility. Fixes issue #296.

--- trunk/age/src/eu/irreality/age/util/UTF8PropertiesLoader.java
+++ trunk/age/src/eu/irreality/age/util/UTF8PropertiesLoader.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -24,6 +26,37 @@ public class UTF8PropertiesLoader
 	{ 
 	    return new Scanner(is).useDelimiter("\\A").next();
 	}
+	
+	
+	/**
+	 * Converts the given string to ISO-8859-1, in such a way that if a character of the source string is not representable in that charset, it is
+	 * escaped. 
+	 * @param src
+	 * @return
+	 */
+	public static String toIsoWithEscapes( String src ) 
+	{
+	    //final String src = "Hallo הצü"; // this has to be read with the right encoding
+	    final CharsetEncoder asciiEncoder = Charset.forName("ISO-8859-1").newEncoder(); //US-ASCII?
+	    final StringBuilder result = new StringBuilder();
+	    char[] arr = src.toCharArray();
+	    for ( int i = 0 ; i < arr.length ; i++ ) 
+	    {
+	    	char character = arr[i];
+	        if (asciiEncoder.canEncode(character)) 
+	        {
+	            result.append(character);
+	        } 
+	        else 
+	        {
+	            result.append("\\u");
+	            result.append(Integer.toHexString(0x10000 | character).substring(1).toUpperCase());
+	        }
+	    }
+	    return result.toString();
+	 }
+	
+	
 	
 	/**
 	 * Loads properties from a custom encoding properties file, in an 1.5-compatible way.
@@ -45,7 +78,8 @@ public class UTF8PropertiesLoader
 		  }
 		} while (read>=0);
 		String propertiesAsString = out.toString();
-		ByteArrayInputStream bais = new ByteArrayInputStream(propertiesAsString.getBytes("ISO-8859-1"));
+		//ByteArrayInputStream bais = new ByteArrayInputStream(propertiesAsString.getBytes("ISO-8859-1"));
+		ByteArrayInputStream bais = new ByteArrayInputStream(toIsoWithEscapes(propertiesAsString).getBytes());
 		p.load(bais);
 		bais.close();
 		in.close();
