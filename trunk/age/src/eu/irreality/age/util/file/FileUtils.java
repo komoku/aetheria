6a592259222326d389e35a20349ae2a6562f9b76
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2013-04-27 18:16:42 +0000

    Started coded (and done quite a bit) the "Export applet" functionality for PUCK.

--- trunk/age/src/eu/irreality/age/util/file/FileUtils.java
+++ trunk/age/src/eu/irreality/age/util/file/FileUtils.java
@@ -1,10 +1,13 @@
 package eu.irreality.age.util.file;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.channels.FileChannel;
 
 public class FileUtils 
 {
@@ -58,6 +61,40 @@ public class FileUtils
 	}
 	
 	*/
+	
+	public static void copyFile(File sourceFile, File destFile) throws IOException 
+	{
+		
+	    if (!destFile.exists() )
+	    {
+	        destFile.createNewFile();
+	    }
+
+	    FileChannel source = null;
+	    FileChannel destination = null;
+	    try 
+	    {
+	        source = new FileInputStream(sourceFile).getChannel();
+	        destination = new FileOutputStream(destFile).getChannel();
+
+	        // previous code: destination.transferFrom(source, 0, source.size());
+	        // to avoid infinite loops, should be:
+	        long count = 0;
+	        long size = source.size();              
+	        while((count += destination.transferFrom(source, count, size-count))<size);
+	    }
+	    finally 
+	    {
+	        if(source != null) 
+	        {
+	            source.close();
+	        }
+	        if(destination != null) 
+	        {
+	            destination.close();
+	        }
+	    }
+	}
 	
 	
 	
