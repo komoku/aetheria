058b928433e169b50a3d2ad768a62c948ff6da97
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2013-07-20 18:10:35 +0000

    Preliminary support for complex (supporting percents, etc.) frame width/height specifications with MigLayout in ColoredSwingClient. We can now do things like addFrame(ImageConstants.RIGHT,"25%:25%:25%").

--- trunk/age/conf/scripts/age-catalog.bat
+++ trunk/age/conf/scripts/age-catalog.bat
@@ -1,4 +1,4 @@
 echo off
 echo Llamando a la Java (tm) Virtual Machine para ejecutar Aetheria...
-start javaw -Xmx512M -classpath .;AgeCore.jar;lib/bsh-2.0b4.jar;lib/micromod.jar;lib/commons-logging-api.jar;lib/jl1.0.jar;lib/jogg-0.0.7.jar;lib/jorbis-0.0.15.jar;lib/jspeex0.9.7.jar;lib/tritonus_share.jar;lib/vorbisspi1.0.2.jar;lib/mp3spi1.9.4.jar;lib/basicplayer3.0.jar;lib/svgSalamander.jar;lib/commons-cli-1.2.jar eu.irreality.age.swing.newloader.NewLoader --errorlog logs\age-catalog.log %* > NUL 
+start javaw -Xmx512M -classpath .;AgeCore.jar;lib/bsh-2.0b4.jar;lib/micromod.jar;lib/commons-logging-api.jar;lib/jl1.0.jar;lib/jogg-0.0.7.jar;lib/jorbis-0.0.15.jar;lib/jspeex0.9.7.jar;lib/tritonus_share.jar;lib/vorbisspi1.0.2.jar;lib/mp3spi1.9.4.jar;lib/basicplayer3.0.jar;lib/svgSalamander.jar;lib/commons-cli-1.2.jar;lib/miglayout-core-4.2.jar;lib/miglayout-swing-4.2.jar eu.irreality.age.swing.newloader.NewLoader --errorlog logs\age-catalog.log %* > NUL 
 rem 2>> simpleage.log
