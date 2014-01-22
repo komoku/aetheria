ac9e8cfea95f4f68d791b95f51ef746e7cc483ee
komoku@gmail.com <komoku@gmail.com@71913296-a87f-df44-316f-665e2712eadf>
2014-01-22 22:03:53 +0000

    AGE's filter for world files now accepts files of the form *.agw (new world extension) and *world*.xml (legacy form).

--- trunk/age/src/eu/irreality/age/FiltroFicheroMundo.java
+++ trunk/age/src/eu/irreality/age/FiltroFicheroMundo.java
@@ -10,6 +10,9 @@ public class FiltroFicheroMundo extends javax.swing.filechooser.FileFilter
 	public boolean accept ( File f )
 	{
 		if ( f.getName().equalsIgnoreCase("world.dat") || f.getName().equalsIgnoreCase("world.xml") ) return true;
+		// Aunque esta condición (equivalente a *world*.xml) captura también el caso de world.xml, dejamos la anterior como "fast path".
+		else if ( f.getName().endsWith(".xml") && f.getName().toLowerCase().contains("world") ) return true;
+		else if ( f.getName().endsWith(".agw") ) return true;
 		else if ( f.getName().endsWith(".agz") ) return true;
 		else if ( !f.isFile() ) return true;
 		else return false;
