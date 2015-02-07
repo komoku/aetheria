80632b16008bbfb87c2b9e4249f57423514f4968
komoku@gmail.com <komoku@gmail.com@71913296-a87f-df44-316f-665e2712eadf>
2015-02-07 10:18:06 +0000

    PUCK now saves files with .agw file extension by default. Related: issue #6.

--- trunk/age/src/eu/irreality/age/FiltroFicheroMundo.java
+++ trunk/age/src/eu/irreality/age/FiltroFicheroMundo.java
@@ -7,13 +7,19 @@ import eu.irreality.age.i18n.UIMessages;
 public class FiltroFicheroMundo extends javax.swing.filechooser.FileFilter
 {
 	
-	public boolean accept ( File f )
+	public boolean acceptFilename ( File f )
 	{
 		if ( f.getName().equalsIgnoreCase("world.dat") || f.getName().equalsIgnoreCase("world.xml") ) return true;
 		// Aunque esta condición (equivalente a *world*.xml) captura también el caso de world.xml, dejamos la anterior como "fast path".
 		else if ( f.getName().endsWith(".xml") && f.getName().toLowerCase().contains("world") ) return true;
 		else if ( f.getName().endsWith(".agw") ) return true;
 		else if ( f.getName().endsWith(".agz") ) return true;
+		else return false;
+	}
+	
+	public boolean accept ( File f )
+	{
+		if ( acceptFilename(f) ) return true;
 		else if ( !f.isFile() ) return true;
 		else return false;
 	}
