69a8a4d404838d50efe15f2705dad95f3e033b32
komoku <komoku@71913296-a87f-df44-316f-665e2712eadf>
2015-02-08 12:57:34 +0000

    Version numbering bumped to 1.3.5.

--- trunk/puck/src/org/f2o/absurdum/puck/gui/panels/WorldPanel.java
+++ trunk/puck/src/org/f2o/absurdum/puck/gui/panels/WorldPanel.java
@@ -137,7 +137,7 @@ public class WorldPanel extends GraphElementPanel implements BeanShellCodeHolder
 	private HashMap colorsMap = new HashMap();
 	private HashMap buttonsMap = new HashMap();
 	
-	public static String DEFAULT_AGE_VERSION = "1.3.4";
+	public static String DEFAULT_AGE_VERSION = "1.3.5";
 	
 	public void setDefaultWorldVersion()
 	{
